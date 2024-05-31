d32447ddee7e39f5ea2fb796996253905de34de8
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-05-31 10:15:20 +0530

    TimeLine Updates

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -33,13 +33,18 @@ const timelineData = [
   },
 ];
 
+// 1. "Start Your Journey with Sho-ol: Easy Signup, Quick Setup, Endless Possibilities!"
+// 2. "Personalize Your Dashboard: Select the Pages You Need, Customize Your Sho-ol Journey!!"
+// 3. "Empower Your Team: Invite, Assign Roles, Collaborate Seamlessly!"
+// 4. "Monitor Success Effortlessly: Comprehensive Analytics, Focus on What Matters!"
+
 const TimelineComp = () => {
   return (
     <Box className="text-center w-full h-full flex-col flex items-center justify-center p-20">
       <Box className="font-bold text-[28px] border-b-2 border-[#4D44B5] mb-10">
         How Do We Work?
       </Box>
-      <Box className="w-[90%] flex flex-col">
+      {/* <Box className="w-[90%] flex flex-col">
         {timelineData.map((item, index) => (
           <Box
             className={`justify-between w-full flex items-center ${
@@ -52,11 +57,142 @@ const TimelineComp = () => {
               <Box className="text-lg">{item.description}</Box>
             </Box>
             <Box>
-              <Image className="h-[400px] w-[400px]" src={item.image} alt="img1" />
+              <Image
+                className="h-[400px] w-[400px]"
+                src={item.image}
+                alt="img1"
+              />
             </Box>
           </Box>
         ))}
-      </Box>
+      </Box> */}
+      <>
+        {/* //component */}
+        <div className="container mx-auto bg-white p-4 lg:p-12">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+            <div className="overflow-hidden rounded-2xl shadow-md bg-blue-50 p-4 lg:p-12">
+              {/* <div className="flex items-center text-blue-500">
+                <p className="text-sm font-bold uppercase">
+                  Feature 1 &amp; Feature 2
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2 h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </div> */}
+              <h2 className="mt-4 text-xl font-semibold text-slate-800">
+                Start Your Journey with Sho-ol
+              </h2>
+              <p className="mt-4  text-slate-600">
+                Easy Signup, Quick Setup, Endless Possibilities!
+              </p>
+              <div className="mt-12 flex transform items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
+                <Image className="" src={img3} alt="img1" />
+              </div>
+            </div>
+            <div className="overflow-hidden rounded-2xl shadow-md bg-pink-50 p-4 lg:p-12">
+              {/* <div className="flex items-center text-pink-500">
+                <p className="text-sm font-bold uppercase">
+                  Feature 3 &amp; Feature 4
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2 h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </div> */}
+              <h2 className="mt-4 text-xl font-semibold text-slate-800">
+                Personalize Your Dashboard
+              </h2>
+              <p className="mt-4  text-slate-600">
+                Select the Pages You Need, Customize Your Sho-ol Journey!!
+              </p>
+              <div className="mt-12 flex transform items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
+                <Image className="" src={img1} alt="img1" />
+              </div>
+            </div>
+            <div className="overflow-hidden rounded-2xl shadow-md bg-green-50 p-4 lg:p-12">
+              {/* <div className="flex items-center text-green-500">
+                <p className="text-sm font-bold uppercase">
+                  Feature 5 &amp; Feature 6
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2 h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </div> */}
+              <h2 className="mt-4 text-xl font-semibold text-slate-800">
+                Empower Your Team
+              </h2>
+              <p className="mt-4  text-slate-600">
+                Invite, Assign Roles, Collaborate Seamlessly!
+              </p>
+              <div className="mt-12 flex transform items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
+                <Image className="" src={img4} alt="img1" />
+              </div>
+            </div>
+            <div className="overflow-hidden rounded-2xl shadow-md bg-purple-50 p-4 lg:p-12">
+              {/* <div className="flex items-center text-purple-500">
+                <p className="text-sm font-bold uppercase">
+                  Feature 7 &amp; Feature 8
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2 h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </div> */}
+              <h2 className="mt-4 text-xl font-semibold text-slate-800">
+                Monitor Success Effortlessly
+              </h2>
+              <p className="mt-4  text-slate-600">
+                Comprehensive Analytics, Focus on What Matters!
+              </p>
+              <div className="mt-12 flex transform items-center justify-center transition-transform duration-300 ease-in-out hover:scale-105">
+                <Image className="" src={img2} alt="img1" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     </Box>
   );
 };
