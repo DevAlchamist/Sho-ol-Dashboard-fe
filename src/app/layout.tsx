d1526099146d1cf12bbbb34687ff7efcbca22cd9
108developer <108developer@gmail.com>
2024-10-19 17:52:58 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,20 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Extended Metadata for SEO
 export const metadata: Metadata = {
   title: "Sho-ol",
   description: "Comprehensive school dashboard website offering features like student records, event management, and more.",
   keywords: "school management, student records, event management, Sho-ol, dashboard, education platform",
   abstract: "Sho-ol is a school management platform designed to streamline administrative tasks, student records, and scheduling.",
 
-  // Open Graph Metadata (used by Facebook, LinkedIn, etc.)
   openGraph: {
     title: "Sho-ol: School Management Dashboard",
     description: "A comprehensive school management platform for student records, event scheduling, and more.",
-    url: "https://sho-ol-dashboard-fe.vercel.app", // Update this to your actual website URL
+    url: "https://sho-ol.vercel.app",
     type: "website",
     images: [
       {
-        url: 'https://i.imgur.com/7tIn3Ab.png', // Using imported image
+        url: 'https://i.imgur.com/7tIn3Ab.png',
         width: 1200,
         height: 630,
         alt: "Sho-ol Dashboard",
@@ -36,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
